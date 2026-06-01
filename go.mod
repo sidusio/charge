1dc49b529211ba850655b80b57e560b2e03b22b9
Karl Wikström <karl@wikstrom.io>
2026-06-01 18:48:51 +0200

    feat: better listenAndServe

--- go.mod
+++ go.mod
@@ -1,5 +1,7 @@
 module sidus.io/notman
 
-go 1.24.13
+go 1.25.0
 
 require github.com/kelseyhightower/envconfig v1.4.0
+
+require golang.org/x/sync v0.20.0
---
+++ internal/http/listen.go
@@ -0,0 +1,33 @@
+package http
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func ListenAndServe(ctx context.Context, server *http.Server, shutdownTimeout time.Duration) error {
+	eg, ctx := errgroup.WithContext(ctx)
+
+	eg.Go(func() error {
+		<-ctx.Done()
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
+		defer cancel()
+
+		return server.Shutdown(shutdownCtx)
+	})
+
+	eg.Go(func() error {
+		err := server.ListenAndServe()
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			return fmt.Errorf("failed to listen and serve: %w", err)
+		}
+		return nil
+	})
+
+	return eg.Wait()
+}
