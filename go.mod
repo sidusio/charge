196619647ef8290468877ad079f6e438be5612b8
Karl Wikström <karl@wikstrom.io>
2026-06-01 21:30:42 +0200

    wip jwk

--- go.mod
+++ go.mod
@@ -1,7 +1,22 @@
 module sidus.io/notman
 
-go 1.25.0
+go 1.26.0
 
 require github.com/kelseyhightower/envconfig v1.4.0
 
-require golang.org/x/sync v0.20.0
+require (
+	github.com/jwx-go/jwkfetch/v4 v4.0.2
+	github.com/lestrrat-go/jwx/v4 v4.0.2
+	golang.org/x/sync v0.20.0
+)
+
+require (
+	github.com/lestrrat-go/blackmagic v1.0.4 // indirect
+	github.com/lestrrat-go/dsig v1.3.0 // indirect
+	github.com/lestrrat-go/httpcc v1.0.1 // indirect
+	github.com/lestrrat-go/httprc/v3 v3.0.5 // indirect
+	github.com/lestrrat-go/option/v2 v2.0.0 // indirect
+	github.com/lestrrat-go/option/v3 v3.0.0-alpha1 // indirect
+	github.com/valyala/fastjson v1.6.10 // indirect
+	golang.org/x/crypto v0.50.0 // indirect
+)
--- internal/app/run.go
+++ internal/app/run.go
@@ -6,27 +6,135 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
+	"net/url"
+	"slices"
+	"sync"
 	"time"
 
+	"github.com/jwx-go/jwkfetch/v4"
+	"github.com/lestrrat-go/jwx/v4/jwt"
 	"golang.org/x/sync/errgroup"
 	"sidus.io/notman/internal/util"
 )
+
+type BackendIndex struct {
+	mu      sync.RWMutex
+	backens map[string]*Backend
+}
+
+func (i *BackendIndex) getBackend(issuer string) *Backend {
+	i.mu.RLock()
+	defer i.mu.RUnlock()
+	return i.backens[issuer]
+}
+
+func (i *BackendIndex) getOrSetBackend(issuer string, new *Backend) *Backend {
+	i.mu.Lock()
+	defer i.mu.Unlock()
+	be, ok := i.backens[issuer]
+	if ok {
+		return be
+	}
+
+	i.backens[issuer] = new
+	return new
+}
+
+func (i *BackendIndex) GetBackend(ctx context.Context, issuer string) (*Backend, error) {
+	be := i.getBackend(issuer)
+	if be != nil {
+		return be, nil
+	}
+
+	be, err := NewBackend(ctx, issuer)
+	if err != nil {
+		return nil, fmt.Errorf("new backend: %w", err)
+	}
+
+	return i.getOrSetBackend(issuer, be), nil
+
+}
+
+type Backend struct {
+	board     *SwitchBoard
+	eventsURL url.URL
+}
+
+func NewBackend(ctx context.Context, issuer string) (*Backend, error) {
+	cache, err := jwkfetch.NewCache(ctx, jwkfetch.NewClient())
+
+	return nil, nil
+}
+
+func (b *Backend) KeySet() {
+
+}
+
+func (b *Backend) Connect(ctx context.Context, id string, signals chan<- Signal) error {
+	b.board.Register(id, signals)
+
+	_, err := http.Post(b.eventsURL.String(), "text/plain", bytes.NewBufferString("connected"))
+	if err != nil {
+		return fmt.Errorf("post connected: %w", err)
+	}
+
+	return nil
+}
+
+func (b *Backend) Disconnect(ctx context.Context, id string) error {
+	b.board.Unregister(id)
+
+	_, err := http.Post(b.eventsURL.String(), "text/plain", bytes.NewBufferString("disconnected"))
+	if err != nil {
+		return fmt.Errorf("post disconnected: %w", err)
+	}
+
+	return nil
+}
 
 func Run(ctx context.Context, log *slog.Logger, cfg Config) error {
 	eg, ctx := errgroup.WithContext(ctx)
 
 	mux := http.NewServeMux()
 
-	board := &SwitchBoard{
-		connections: make(map[string]chan<- Signal),
+	bi := BackendIndex{
+		backens: make(map[string]*Backend),
 	}
 
 	mux.HandleFunc("/sse", func(w http.ResponseWriter, r *http.Request) {
 		ctx = r.Context()
+
+		rawToken := r.URL.Query().Get("token")
+		if rawToken == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		unsafeToken, err := jwt.ParseInsecure([]byte(rawToken))
+		if err != nil {
+			log.Warn("Failed to parse token", "error", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		audience, _ := unsafeToken.Audience()
+		if !slices.Contains(audience, cfg.DeploymentIdentifier) {
+			log.Warn("Invalid audience", "audience", audience)
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
+
+		issuer, _ := unsafeToken.Issuer()
+
+		be, err := bi.GetBackend(ctx, issuer)
+		if err != nil {
+			w.WriteHeader(http.StatusServiceUnavailable)
+			return
+		}
+
+		token, err := jwt.Parse([]byte(rawToken), jwt.WithVerifyAuto(be.KeySet()))
+
 		// Read ?token
 		// Figure out who is backend
-
-		issuer := ""
 
 		id := "abc"
 
@@ -34,15 +142,8 @@ func Run(ctx context.Context, log *slog.Logger, cfg Config) error {
 		signals := make(chan Signal)
 		defer close(signals)
 
-		// Register channel in index
-		board.Register(id, signals)
-		// defer unregistartion from index
-		defer board.Unregister(id)
-
-		// POST /connected
-		http.Post(fmt.Sprintf("%s/events", issuer), "text/plain", bytes.NewBufferString("connected"))
-		// defer POST /disconnected
-		defer http.Post(fmt.Sprintf("%s/events", issuer), "text/plain", bytes.NewBufferString("disconnected"))
+		be.Connect(ctx, id, signals)
+		defer be.Disconnect(ctx, id)
 
 		// Loop over channel and write
 		for {
